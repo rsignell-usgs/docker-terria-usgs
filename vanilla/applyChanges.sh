bc3fe090dd517e76a64e91ff665b396baff2acfd
Zafer <zdefne-usgs@users.noreply.github.com>
2018-04-13 21:17:11 -0400

    Vanilla for USGS

---
+++ vanilla/applyChanges.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /opt/docker
+docker-compose down
+docker rmi -f terria-local
+docker rm $(docker ps -qa –no-trunc –filter "status=exited")
+docker rmi $(docker images –filter "dangling=true" -q –no-trunc)
+docker ps
+docker images
+cd ~/Docker
+docker build -t terria-local .
+cd /opt/docker
+docker-compose up -d
